c2f0ff0a94f366a84b1d35ae391e7f63d1cebefa
eugene_prikhach <eugeneprikhach@icloud.com>
2020-11-21 00:01:52 +0300

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+C
+winequality-red.csv,9/6/968f1cd7ab07ce59f0c2a4dab92a1c90a6efab1d
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
---
+++ main.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+
+
+data_train = pd.read_csv('winequality-red.csv', delimiter=",")
+print('Number of rows and columns in initial dataset:', data_train.shape)
+train, validation = train_test_split(data_train, test_size=0.2)
+arr_name = []
+arr_train = []
+arr_val = []
+# используем предварительно отобранные признаки
+cols_x = ['volatile acidity', 'alcohol', 'citric acid', 'sulphates']
+# целевой признак
+col_y = 'quality'
+
+
+def show_correlation_hitmap():
+    sns.set(style="ticks")
+    sns.heatmap(data_train.corr())
+    plt.show()
+
+
+def classifier_testing(classifier, classifier_name):
+    # обучаем классификатор
+    classifier.fit(train[cols_x], train[col_y])
+    # проверяем классификатор
+    y_train = classifier.predict(train[cols_x])
+    # определяем точность
+    y_train_acc = accuracy_score(train[col_y], y_train)
+    # для валидационной выборки
+    y_val = classifier.predict(validation[cols_x])
+    y_val_acc = accuracy_score(validation[col_y], y_val)
+    # сохранение информации в массивы
+    arr_name.append(classifier_name)
+    arr_train.append(y_train_acc)
+    arr_val.append(y_val_acc)
+    # вывод промежуточных результатов
+    print('Accuracy per {} algorithm on train data = {}, on validation data = {}' \
+          .format(classifier_name, \
+                  round(y_train_acc, 3), \
+                  round(y_val_acc, 3)))
+    # возвращаем обученный классификатор
+    return classifier
+
+
+def classifier_testing_comparison():
+    x = range(len(arr_train))
+    plt.plot(x, arr_train)
+    plt.plot(x, arr_val)
+    plt.xticks(x, arr_name)
+    plt.ylabel('algorithm accuracy')
+    plt.legend(['Train', 'Validation'], loc='upper right')
+    plt.show()
+
+
+def predict_model():
+    predicted_good_wine = []
+    is_good_wine = []
+    srwg, srwb, sfwg, sfwb = None
+    clf = DecisionTreeClassifier()
+    clf.fit(train[cols_x], train[col_y])
+    for i in validation[col_y]:
+        is_good_wine.append(i)
+    for i in clf.predict(validation[cols_x]):
+        predicted_good_wine.append(i)
+    for i in range(len(is_good_wine)):
+        for j in is_good_wine, predicted_good_wine:
+            if is_good_wine[i] == predicted_good_wine[i] and predicted_good_wine[i] >= 5:
+                srwg += 1
+            elif is_good_wine[i] == predicted_good_wine[i] and predicted_good_wine[i] <= 5:
+                srwb += 1
+            elif is_good_wine[i] != predicted_good_wine[i] and predicted_good_wine[i] >= 5:
+                sfwg += 1
+            elif is_good_wine[i] != predicted_good_wine[i] and predicted_good_wine[i] <= 5:
+                sfwb += 1
+
+
+
+def main():
+    show_correlation_hitmap()
+    classifier_testing(KNeighborsClassifier(), 'KNN')
+    classifier_testing(GradientBoostingClassifier(), 'GB')
+    classifier_testing(DecisionTreeClassifier(), 'Tree')
+    classifier_testing(SVC(), 'SVM')
+    classifier_testing(LogisticRegression(solver='lbfgs', max_iter=10000), 'LR')
+    classifier_testing_comparison()
+    predict_model()
+
+
+if __name__ == '__main__':
+    main()
